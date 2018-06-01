b6df38fea9f5ce2efb25b1ec9fd9bd2151ff0590
Jesse Shaw <jdshaw1987@gmail.com>
2018-05-31 22:57:36 -0400

    Updated README

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ app.get("/", (req, res)=>
 );
 
 app.get("/item/:id", (req, res, next)=>{
+    //This is the middleware that pulls data
     let user = Number(req.params.id) - 1;
     console.log(data[user]);
+    //middleware that uses the req object
+    console.log(`Request from: ${req.originalUrl}`)
+    console.log(`Request type: ${req.method}`)
+    //everything above is middleware
     res.send(data[user])
     next();
 }, (req, res)=> {
@@ -24,7 +29,7 @@ app.get("/item/:id", (req, res, next)=>{
 });
 
 app.get('/images', (req, res)=>{
-    res.download("images/rocket.jpg")
+    // res.download("images/rocket.jpg")
     // res.redirect("https://www.facebook.com/")
     // res.end()
 })
@@ -41,6 +46,19 @@ app.delete('/deleteit', (req, res)=>
     res.send("a delete request has been made")
 )
 
+/*Chaining */
+
+app.route('/chain').get((req, res) => {
+    res.send("A get request was made");
+}).put((req, res) => {
+    res.send("A Put Request was made")
+    console.log("Put It")
+}).delete((req, res) => {
+    res.send("A Delete Request was made")
+    console.log("Delete It")  
+})
+
+/*PORT */
 app.listen(PORT, ()=>{
     console.log(`Your server is running on port ${PORT}`);
 })
